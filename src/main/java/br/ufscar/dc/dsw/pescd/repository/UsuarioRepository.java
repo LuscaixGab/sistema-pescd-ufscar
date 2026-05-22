f8f4a70e6288edd1ba8413417d8d17c8d896e5cd
Lucas Gabriel <lucas0403valenti@outlook.com>
2026-05-22 19:42:42 -0300

    Adiciona UsuarioRepository

---
+++ src/main/java/br/ufscar/dc/dsw/pescd/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.ufscar.dc.dsw.pescd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.ufscar.dc.dsw.pescd.model.Usuario; // Ajuste para o nome do seu pacote
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}
